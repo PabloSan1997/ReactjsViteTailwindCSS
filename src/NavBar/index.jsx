0920f6932a41f9824ad35d4722fcc3a595b3da10
PabloSan1997 <jos_pab_997@hotmali.com>
2023-05-16 16:51:23 -0600

    Se mejoro el nav

--- src/NavBar/index.jsx
+++ src/NavBar/index.jsx
@@ -3,29 +3,30 @@ import { NavLink } from "react-router-dom";
 
 
 export function Navbar() {
+    const activeStyle = "underline underline-offset-4";
     return (
         <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
             <ul className="flex items-center gap-3">
-                <li className="font-semibold">
+                <li className="font-semibold ">
                     <NavLink to="/">Shopi</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/">All</NavLink>
+                    <NavLink to="/" className={({ isActive }) => isActive ? activeStyle : undefined}>All</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/clothes">Clothes</NavLink>
+                    <NavLink to="/clothes" className={({ isActive }) => isActive ? activeStyle : undefined}>Clothes</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/electronics">Electronic</NavLink>
+                    <NavLink to="/electronics" className={({ isActive }) => isActive ? activeStyle : undefined}>Electronic</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/furnitures">Fornitures</NavLink>
+                    <NavLink to="/furnitures" className={({ isActive }) => isActive ? activeStyle : undefined}>Fornitures</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/toys">Toys</NavLink>
+                    <NavLink to="/toys" className={({ isActive }) => isActive ? activeStyle : undefined}>Toys</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/others">Others</NavLink>
+                    <NavLink to="/others" className={({ isActive }) => isActive ? activeStyle : undefined}>Others</NavLink>
                 </li>
             </ul>
             <ul className="flex items-center gap-3">
@@ -33,13 +34,13 @@ export function Navbar() {
                     hola@hotmail.com
                 </li>
                 <li>
-                    <NavLink to="/my-orders">My orders</NavLink>
+                    <NavLink to="/my-orders" className={({ isActive }) => isActive ? activeStyle : undefined}>My orders</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/my-account">My Account</NavLink>
+                    <NavLink to="/my-account" className={({ isActive }) => isActive ? activeStyle : undefined}>My Account</NavLink>
                 </li>
                 <li>
-                    <NavLink to="/signin">Sign in</NavLink>
+                    <NavLink to="/signin" className={({ isActive }) => isActive ? activeStyle : undefined}>Sign in</NavLink>
                 </li>
                 <li>
                     🛒 0
